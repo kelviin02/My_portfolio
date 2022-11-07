1b72dafc401f99184a9bb736ef47db5862049ea2
kelviin02 <laanrekelipher67@gmail.com>
2022-11-07 17:19:33 +0100

    Added javascript

---
+++ index.js
@@ -0,0 +1,38 @@
+//MOVING TEXT
+var options = {
+    strings: ['<i>Developer</i> .', 'Programmer.',"Freelancer."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  };
+  
+  var typed = new Typed('#auto-type', options);
+
+
+//NAV-BARS
+  const navBar = document.querySelector(".fa-bars")
+  const newNav = document.addEventListener("click", toggleIcon)
+
+  function toggleIcon(){
+      navBar.classList.toggle("fa-bars")
+      navBar.classList.toggle("fa-times")
+  }
+
+
+//BACK TO-TOP ARROW  
+  const topIcon = document.querySelector('.to-top')
+        window.addEventListener('scroll', showIcon)
+
+        topIcon.addEventListener('click', toTop)
+
+        function showIcon() {
+            if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+                topIcon.style.display = "block"
+            } else {
+                topIcon.style.display = "none"
+            }
+        }
+
+        function toTop() {
+            document.documentElement.scrollTop = 0
+        }
