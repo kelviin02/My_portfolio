3635d6ca90d95d24577b304c4ddf6f37649c3261
kelviin02 <laanrekelipher67@gmail.com>
2022-11-17 13:31:51 +0100

    added new changes

--- index.js
+++ index.js
@@ -23,5 +23,18 @@ var options = {
     loop: true
   };
   
-  var typed = new Typed('#auto-type', options);
+var typed = new Typed('#auto-type', options);
 
+
+//NAV ON ACTIVE
+const li = document.querySelectorAll(".nav-li")
+const sec = document.querySelectorAll("section")
+
+   function activeMenu() {
+    let len = sec.length
+    while(--len && window.scrollY +97 < sec[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove("active"))
+    li[len].classList.add("active")
+   }
+activeMenu()
+   window.addEventListener("scroll", activeMenu)
